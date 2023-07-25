9e0639b6f1a8ae9ffaea5cf8f0a4189cd69e5659
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-07-25 16:47:04 +0200

    page detail/pagelist mockups

--- src/PageDetail.js
+++ src/PageDetail.js
@@ -3,11 +3,80 @@ export const PageDetail = (argument) => {
     const cleanedArgument = argument.trim().replace(/\s+/g, "-");
 
     const displayGame = (gameData) => {
-      const { name, released, description } = gameData;
+      const {
+        name,
+        released,
+        description,
+        background_image,
+        tags,
+        genres,
+        publishers,
+        rating,
+        developers,
+        platforms,
+        stores,
+      } = gameData;
       const articleDOM = document.querySelector(".page-detail .article");
-      articleDOM.querySelector("h1.title").innerHTML = name;
-      articleDOM.querySelector("p.release-date span").innerHTML = released;
-      articleDOM.querySelector("p.description").innerHTML = description;
+      // articleDOM.querySelector("h1.title").innerHTML = name;
+      // articleDOM.querySelector("p.release-date span").innerHTML = released;
+      // articleDOM.querySelector("p.description").innerHTML = description;
+      articleDOM.innerHTML = `
+        <div class="info">
+          <img src="${background_image}" class="banner">
+          <div class="description">
+            <h2>${name},</h2>
+            <p>${description}</p>
+          </div>
+          <div class="makers">
+            <div>
+              <p><b>Release Date</b></p>
+              <p>${released}</p>
+            </div>
+
+            <div>
+              <p><b>Developer</b></p>
+              <p>${developers.map((dev) => dev.name).join(", ")}</p>
+            </div>
+
+            <div>
+              <p><b>Platforms</b></p>
+              <p>${platforms
+                .map((platform) => platform.platform.name)
+                .join(", ")}</p>
+            </div>
+
+            <div>
+              <p><b>Publisher</b></p>
+              <p>${publishers.map((publisher) => publisher.name).join(", ")}</p>
+            </div>
+          </div>
+
+          <div class ="etc">
+            <div>
+              <p><b>Genre</b></p>
+              <p>${genres.map((genre) => genre.name).join(", ")}</p>
+            </div>
+
+            <div>
+              <p><b>Tags</b></p>
+              <p>${tags.map((tag) => tag.name).join(", ")}</p>
+            </div>
+          </div>
+          <div class ="stores">
+            <div>
+              <h2>BUY</h2>
+              <p>${stores
+                .map(
+                  (store) =>
+                    `<a href="https://www.${store.store.domain}/game/${gameData.name}" target="_blank">${store.store.name}</a><br>`
+                )
+                .join("")}
+              </p>
+
+            </div>
+          </div>
+        </div>
+      `;
     };
 
     const fetchGame = (url, argument) => {
@@ -25,9 +94,6 @@ export const PageDetail = (argument) => {
     pageContent.innerHTML = `
       <section class="page-detail">
         <div class="article">
-          <h1 class="title"></h1>
-          <p class="release-date">Release date : <span></span></p>
-          <p class="description"></p>
         </div>
       </section>
     `;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-// import "./style/index.scss"; // Attention ici, il faut bien mettre l'extension `.scss`
+import "./style/index.scss"; // Attention ici, il faut bien mettre l'extension `.scss`
 import { routes } from "./routes.js";
+import { Home } from "./Home.js";
+
+const renderHome = () => {
+  const contentContainer = document.getElementById("pageContent");
+  contentContainer.innerHTML = Home();
+};
 
 const callRoute = () => {
+  renderHome();
+
   const { hash } = window.location;
   const pathParts = hash.substring(1).split("/");
 
-  const pageName = pathParts[0].toLowerCase(); // Convert to lowercase for consistency
+  const pageName = pathParts[0].toLowerCase();
   const pageArgument = pathParts[1] || "";
   const pageFunction = routes[pageName];
 
